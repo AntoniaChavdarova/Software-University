689173d7e7e6c09b4cb9e612fc2cb52c9188cf86
AntoniaChavdarova <niaaachavdarova@abv.bg>
2021-01-16 22:36:14 +0200

    Initial Update

---
+++ ImplementingCustomDataStructure/ImplementingCustomDataStructure/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImplementingCustomDataStructure
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                stack.Push(i);
+            }
+
+            stack.ForEach(e =>
+            {
+                Console.WriteLine(e);
+            }
+            );
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(stack.Peek());
+                Console.WriteLine(stack.Pop());
+            }
+
+            stack.Pop();
+        }
+    }
+}
